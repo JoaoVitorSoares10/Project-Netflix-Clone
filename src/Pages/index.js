639eaa8dd4c63df327e15eee87424c5e1bb4cc5b
Joao Vitor de Morais Soares <joaovitormoraissoares@gmail.com>
2021-12-12 15:56:36 -0300

    card configuration

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import '../Style/index.css';
 
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 export default function Index(){
     return(
         <div className='indexPage'>
-            <div className='indexPage--header'></div>
+            <div className='indexPage--header'>
+                <img alt="Netflix logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"></img>
+                <div className='indexPage--headerButton'>                   
+                    <div class="indexPage--headerDropdown">
+                        <button class="indexPage--headerDropdownDropbtn">
+                            <LanguageIcon style={{fontSize: 16}}/>
+                            <p>Português</p> 
+                            <ExpandMoreIcon style={{fontSize: 16}}/>
+                        </button>
+                        <div class="indexPage--headerDropdownContent">
+                            <a href="/">Português</a>
+                            <a href="/">English</a>
+                        </div>
+                    </div>
+                    <div className='indexPage--headerEnter'>
+                        <a href='/'>Entrar</a>
+                    </div>
+                </div> 
+            </div>
             <div className='indexPage--card'>
                 <div className='indexPage--cardTransparency'>
-                    <div className='indexPage--cardTransparencyTop'>
-                        <div className='indexPage--cardTransparencyBottom'></div>
+                    <div className='indexPage--cardTransparencyTop'></div>
+                    <div className='indexPage--cardInfo'>
+                        <div className='indexPage--cardInfoText'>
+                            <h1>Filmes, series e muito mais.<br/> Sem limites.</h1>
+                            <h2>Assista onde quiser. Cancele quando quiser.</h2>
+                            <h3>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</h3>
+                        </div>
+                        <div className='indexPage--cardInfoForm'>
+                            <input type="text" placeholder='Email'></input>
+                            <a href='/'>Vamos lá {'>'}</a>
+                        </div>
                     </div>
                 </div>
             </div>
