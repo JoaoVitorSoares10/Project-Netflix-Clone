e101accef68bf97e5bcf7ee362de9e71e853a565
Joao Vitor de Morais Soares <joaovitormoraissoares@gmail.com>
2022-01-25 21:09:14 -0300

    Cleaning projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from "./Pages/Home"
-import Index from "./Pages";
+
 import './Style/App.css'
 export default function App(){
   return(
-    <Index />
+    <Home />
   )
 }
